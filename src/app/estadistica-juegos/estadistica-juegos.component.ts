f92b9ee92a837a01c4b633c8c79ef51fe9da44b4
Jorge Pallol <jorgepallol1@gmail.com>
2024-07-21 10:59:33 +0200

    Avances en el frontend

--- src/app/estadistica-juegos/estadistica-juegos.component.ts
+++ src/app/estadistica-juegos/estadistica-juegos.component.ts
@@ -24,7 +24,97 @@ export class EstadisticaJuegosComponent implements AfterViewInit {
   }
 
 
+  private obtenerGenerosporJuegos(): void {
+    const url = 'http://localhost:8080/v1/nbJuegosPorGenero';
 
+  this.http.get(url).subscribe(
+    (response:any) => {
+
+      const nombres:any = [];
+      const cantidades:any = [];
+
+      Object.entries(response).forEach(([nombre, cantidad]) => {
+        nombres.push(nombre);
+        cantidades.push(cantidad);
+      })
+
+      console.log(nombres);
+      console.log(cantidades);
+
+      const ctx = document.getElementById('primera-grafica') as HTMLCanvasElement;
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: nombres,
+          datasets: [
+            {
+              label: 'Comics de cada autor',
+              data: cantidades,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',   // Rojo claro
+                'rgba(54, 162, 235, 0.2)',   // Azul claro
+                'rgba(255, 206, 86, 0.2)',   // Amarillo claro
+                'rgba(75, 192, 192, 0.2)',   // Verde agua
+                'rgba(153, 102, 255, 0.2)',  // Púrpura claro
+                'rgba(255, 159, 64, 0.2)',   // Naranja claro
+                'rgba(255, 99, 255, 0.2)',   // Rosa claro
+                'rgba(54, 162, 100, 0.2)',   // Verde claro
+                'rgba(255, 206, 200, 0.2)',  // Melón claro
+                'rgba(75, 50, 192, 0.2)'     // Azul oscuro
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',     // Rojo
+                'rgba(54, 162, 235, 1)',     // Azul
+                'rgba(255, 206, 86, 1)',     // Amarillo
+                'rgba(75, 192, 192, 1)',     // Verde agua
+                'rgba(153, 102, 255, 1)',    // Púrpura
+                'rgba(255, 159, 64, 1)',     // Naranja
+                'rgba(255, 99, 255, 1)',     // Rosa
+                'rgba(54, 162, 100, 1)',     // Verde
+                'rgba(255, 206, 200, 1)',    // Melón
+                'rgba(75, 50, 192, 1)'       // Azul oscuro
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1, // Define el tamaño del paso a 1
+                callback: (value:any) => {
+                  if (Number.isInteger(value)) {
+                    return value;
+                  }
+                }
+              },
+              grid: {
+                display: false // Elimina la cuadrícula del fondo
+              }
+            },
+            x: {
+              grid: {
+                display: false // Elimina la cuadrícula del fondo
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              display: false // Elimina la leyenda
+            }
+          }
+        }
+      });
+      
+    },
+    (error:any) => {
+      console.error('Error al obtener los animes:', error);
+    }
+  );
+
+}
 
   private pintarMapaWords() {
 
@@ -74,6 +164,7 @@ export class EstadisticaJuegosComponent implements AfterViewInit {
 
   ngAfterViewInit() {
     this.pintarMapaWords();
+    this.obtenerGenerosporJuegos();
   }
 
 }
--- src/app/estadistica-libros/estadistica-libros.component.ts
+++ src/app/estadistica-libros/estadistica-libros.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import Chart from 'chart.js/auto';
 import { WordCloudChart } from 'chartjs-chart-wordcloud';
 
 @Component({
@@ -55,8 +56,102 @@ export class EstadisticaLibrosComponent implements AfterViewInit {
 
   }
   
+  private obtenerComicsDeCadaAutor(): void {
+    const url = 'http://localhost:8080/v1/nbLibrosPorAutor';
+
+  this.http.get(url).subscribe(
+    (response:any) => {
+
+      const nombres:any = [];
+      const cantidades:any = [];
+
+      Object.entries(response).forEach(([nombre, cantidad]) => {
+        nombres.push(nombre);
+        cantidades.push(cantidad);
+      })
+
+      console.log(nombres);
+      console.log(cantidades);
+
+      const ctx = document.getElementById('primera-grafica') as HTMLCanvasElement;
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: nombres,
+          datasets: [
+            {
+              label: 'Comics de cada autor',
+              data: cantidades,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',   // Rojo claro
+                'rgba(54, 162, 235, 0.2)',   // Azul claro
+                'rgba(255, 206, 86, 0.2)',   // Amarillo claro
+                'rgba(75, 192, 192, 0.2)',   // Verde agua
+                'rgba(153, 102, 255, 0.2)',  // Púrpura claro
+                'rgba(255, 159, 64, 0.2)',   // Naranja claro
+                'rgba(255, 99, 255, 0.2)',   // Rosa claro
+                'rgba(54, 162, 100, 0.2)',   // Verde claro
+                'rgba(255, 206, 200, 0.2)',  // Melón claro
+                'rgba(75, 50, 192, 0.2)'     // Azul oscuro
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',     // Rojo
+                'rgba(54, 162, 235, 1)',     // Azul
+                'rgba(255, 206, 86, 1)',     // Amarillo
+                'rgba(75, 192, 192, 1)',     // Verde agua
+                'rgba(153, 102, 255, 1)',    // Púrpura
+                'rgba(255, 159, 64, 1)',     // Naranja
+                'rgba(255, 99, 255, 1)',     // Rosa
+                'rgba(54, 162, 100, 1)',     // Verde
+                'rgba(255, 206, 200, 1)',    // Melón
+                'rgba(75, 50, 192, 1)'       // Azul oscuro
+              ],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              ticks: {
+                stepSize: 1, // Define el tamaño del paso a 1
+                callback: (value:any) => {
+                  if (Number.isInteger(value)) {
+                    return value;
+                  }
+                }
+              },
+              grid: {
+                display: false // Elimina la cuadrícula del fondo
+              }
+            },
+            x: {
+              grid: {
+                display: false // Elimina la cuadrícula del fondo
+              }
+            }
+          },
+          plugins: {
+            legend: {
+              display: false // Elimina la leyenda
+            }
+          }
+        }
+      });
+      
+    },
+    (error:any) => {
+      console.error('Error al obtener los animes:', error);
+    }
+  );
+
+}
+
+
   ngAfterViewInit() {
     this.pintarMapaWords();
+    this.obtenerComicsDeCadaAutor();
   }
 
 }
